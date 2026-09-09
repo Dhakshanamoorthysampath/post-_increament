9c2e78fb261314f2934d4eb38162e62353838e33
Dhakshanamoorthysampath <dhakshanamoorthysampath@gmail.com>
2026-09-08 23:38:02 -0700

    frist commit

---
+++ Prefix.java
@@ -0,0 +1,29 @@
+class Prefix
+{
+	public static void main(String[] args) 
+	{
+	/*	System.out.println("Hello World!");
+		int a= 10;
+		System.out.println(+a);
+		System.out.println(-a);
+		boolean b = true;
+		System.out.println(!b);
+		++a;
+		System.out.println(a);
+		
+		a++;
+		System.out.println(a);
+	*/	
+		int a = 123;
+		int b = 2;
+		
+		System.out.println(a%b);
+		Prefix prefix = new prefix();
+		System.out.println();
+		
+		
+		
+		
+		
+	}
+}
