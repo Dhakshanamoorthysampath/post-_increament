edb95a84f996d62a6d49b786d5892c86e94f6bb6
Dhakshanamoorthysampath <dhakshanamoorthysampath@gmail.com>
2026-09-12 00:49:14 -0700

    second commit

--- increament_decreament/src/com/increament_decreament/IncrementDecrement.java
+++ increament_decreament/src/com/increament_decreament/IncrementDecrement.java
@@ -1,13 +1,15 @@
 package com.increament_decreament;
-
+/**
+ 
+ */
 public class IncrementDecrement {
 
 	public static void main(String[] args) {
 		
 		int a=10;
 		int b=20;
-	//	a++;  //increament a value print a=11
-		a=a++;//now we are a value assain to a but a is apost increament so result is 10
+	//	a++;  //post increament  a=11
+		a=a++;//now we are a value assain to a but a is a post increament so result is 10
 		System.out.println("a= "+a);
 //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 		//hari sir 
